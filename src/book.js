73c261723b7bef3be0999b1af2c23629f21324a8
Matthew Johnson <matthewwmjohnson@gmail.com>
2024-09-30 02:45:21 -0600

    add: buildMainCharacter function.

--- src/book.js
+++ src/book.js
@@ -2,9 +2,17 @@ function createTitle(titleName) {
   return "The " + titleName;
 }
 
+function buildMainCharacter(nameInput, ageInput, pronounsInput) {
+  character = {};
+  character.name = nameInput;
+  character.age = ageInput;
+  character.pronouns = pronounsInput;
+  return character;
+}
+
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
